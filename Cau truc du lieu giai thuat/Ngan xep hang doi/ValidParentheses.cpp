0ac639183b2e7dbafea445c1fe089448d89db21a
nhu220840 <nhu220840@gmail.com>
2023-12-03 22:33:01 +0700

    add file check parentheses using  stack

---
+++ Cau truc du lieu giai thuat/Ngan xep hang doi/ValidParentheses.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+bool isValid(char x, char y){
+    return (x == '(' && y == ')' || x == '{' && y == '}' || x == '[' && y == ']');
+}
+
+bool checkParentheses(string s){
+    stack<int> st;
+    for(int i = 0; i < s.length(); i++){
+        if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+            st.push(s[i]);
+        }
+        else{
+            if(st.empty()) return false;
+            else if(!isValid(st.top(), s[i])) return false;
+            else st.pop();
+        }
+    }
+    return true;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string s; cin >> s;
+    cout << (checkParentheses(s) ? "YES" : "NO");
+}
