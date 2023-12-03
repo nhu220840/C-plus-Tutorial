bd3ed58b4223a6f1366b7423528d35c5656f3a55
nhu220840 <nhu220840@gmail.com>
2023-12-03 21:53:41 +0700

    add file bfs in maze

--- Cau truc du lieu giai thuat/Do thi/DuongDiTrongMeCung.cpp
+++ Cau truc du lieu giai thuat/Do thi/DuongDiTrongMeCung.cpp
@@ -3,25 +3,37 @@
 using namespace std;
 using ll = long long;
 
+#define ii pair<int, int>
+
 int dx[8] = {-1, -1, -1, 0, 1, 1, 1, 0};
 int dy[8] = {-1, 0, 1, 1, 1, 0, -1, -1};
 
 int n;
 int s, t, u, v;
-int a[1005][1005];
-int path = 0;
+int a[1005][1005], path[1005][1005];
 
-void Try(int i, int j){
-    if(i == u && j == v) return;
-    a[i][j] = 0;
-    path++;
+int BFS(){
+    queue<ii> q;
+    q.push({s, t});
+    a[s][t] = 0;
+    path[s][t] = 0;
 
-    for(int k = 0; k < 8; k++){
-        int i1 = i + dx[k], j1 = j + dy[k];
-        if(i1 >= 1 && i1 <= n && j1 >= 1 && j1 <= n && a[i1][j1] == 1){
-            Try(i1, j1);
+    while(!q.empty()){
+        ii top = q.front(); q.pop();
+        int s1 = top.first, s2 = top.second;
+
+        if(s1 == u && s2 == v) return path[u][v];
+        
+        for(int k = 0; k < 8; k++){
+            int i1 = s1 + dx[k], j1 = s2 + dy[k];
+            if(i1 >= 1 && i1 <= n && j1 >= 1 && j1 <= n && a[i1][j1] == 1){
+                q.push({i1, j1});
+                a[i1][j1] = 0;
+                path[i1][j1] = path[s1][s2] + 1;
+            }
         }
     }
+    return -1;
 }
 
 int main(){
@@ -33,9 +45,5 @@ int main(){
             cin >> a[i][j];
         }
     }
-    Try(s, t);
-    if(a[u][v] == 0) cout << -1;
-    else{
-        cout << path;
-    }
+    cout << BFS();
 }
