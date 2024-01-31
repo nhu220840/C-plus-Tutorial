5fc45f40f3e8acc109a74ccc3db6bff257c5ab28
nhu220840 <nhu220840@gmail.com>
2024-01-31 14:38:43 +0700

    update file trash

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,61 +1,11 @@
-#include <bits/stdc++.h>
-#define ll long long
-using namespace std;
-ll MOD = 1e9 + 7;
-int visited[10001];
-int parent[10001];
-bool check = false;
+#include <stdio.h>
+#include <math.h>
+int cal(int a, int b){
 
-void DFS(int x, set<int> adj[])
-{
-    visited[x] = 1;
-    for (auto v : adj[x])
-    {
-        if (!visited[v])
-        {
-            parent[v] = x;
-            DFS(v, adj);
-        }
-        else
-        {
-            if (parent[x] != v && visited[v] == 1)
-            {
-                check = true;
-            }
-        }
-    }
-    visited[x]=2;
+ int res = 2 * a + 3 * b;
+ return res;
 }
-
-int main()
-{
-    // ios_base::sync_with_stdio(NULL);
-    // cin.tie(0);
-    // cout.tie(0);
-    set<int> adj[10001];
-    int n, m;
-    cin >> n >> m;
-    for (int i = 1; i <= m; i++)
-    {
-        int x, y;
-        cin >> x >> y;
-        adj[x].insert(y);
-    }
-    for (int i = 1; i <= n; i++)
-    {
-        if (!visited[i])
-        {
-            DFS(i, adj);
-        }
-    }
-    if (check)
-    {
-        cout << 1 << endl;
-    }
-    else
-    {
-        cout << 0 << endl;
-    }
-
-    return 0;
+int main(){
+ printf("%.2f", (float)cal(10.2, 2.1));
+ return 0;
 }
